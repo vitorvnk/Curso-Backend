d8f98f5009b39dec9eeccfd317e5108fb9ba631f
Vítor Alexandre <devper.vnk@gmail.com>
2021-12-07 18:37:59 -0300

    .

--- Laravel/hdcevents/resources/views/welcome.blade.php
+++ Laravel/hdcevents/resources/views/welcome.blade.php
@@ -2,38 +2,8 @@
 @section('title', 'HDC Events')
 @section('content')
 
-<h1>Algum Título</h1>
-@if(2 > 5)
-    <p>A condição é true</p>
-@endif
-
-<p>{{$nome}}</p>
-
-@if($nome == "Pedro")
-    <p>O nome é {{$nome}}</p>
-@else
-    <p>O nome não é Pedro e sim {{$nome}}. Ele possui {{$idade}} anos.</p>
-@endif
-
-@for($i = 0; $i < count($array); $i++)
-    <a>{{$array[$i]}}</a>
-@endfor
-
-@for($i = 0; $i < 100; $i++)
-    <a>{{$i}}</a>
-@endfor
-
-@php
-    $name = "Vitor";
-    echo $name;
-@endphp
-
-@foreach($nomes as $nome)
-    <p>{{$loop->index}} - {{$nome}}</p>
+@foreach($events as $event)
+    <p>{{$event->title}} -- {{$event->description}}</p>
 @endforeach
-
-
-<!-- Comentário do HTML -->
-{{-- Comentário do Blade, não aparece no Inspecionar Elemento --}}
 
 @endsection
--- Laravel/hdcevents/routes/web.php
+++ Laravel/hdcevents/routes/web.php
@@ -10,16 +10,3 @@ Route::get('/events/create', [EventController::class, 'create']);
 Route::get('/contatos', function () {
     return view('contact');
 });
-
-Route::get('/produtos', function () {
-    $busca = request('search');
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/produtos/{id}', function ($id) {
-    return view('product', ['id' => $id]);
-});
-
-Route::get('/produtosteste/{id?}', function ($id = null) {
-    return view('product', ['id' => $id]);
-});
