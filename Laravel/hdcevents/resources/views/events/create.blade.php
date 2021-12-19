9dee0327d0932b49fe56bb28f8d968b94a32a781
Vítor Alexandre <devper.vnk@gmail.com>
2021-12-19 11:15:07 -0300

    Aula 15

--- Laravel/hdcevents/resources/views/events/create.blade.php
+++ Laravel/hdcevents/resources/views/events/create.blade.php
@@ -2,6 +2,34 @@
 @section('title', 'Criar Evento | HDC Events')
 @section('content')
 
-<h1>Crie um Evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="post">
+        @csrf
+        <div id="form-group col-md-4">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento">
+        </div>
+        <div id="form-group col-md-4">
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do Evento">
+        </div>
+        <div id="form-group col-md-4">
+            <label for="title">O evento é privado?</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div id="form-group col-md-4">
+            <label for="title">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="O que irá acontecer no evento?"></textarea>
+        </div><br>
+        <input type="submit" class="btn btn-primary" value="Guardar">
+    </form>
+</div>
+
+
+
 
 @endsection
--- Laravel/hdcevents/routes/web.php
+++ Laravel/hdcevents/routes/web.php
@@ -7,6 +7,9 @@ Route::get('/', [EventController::class, 'index']);
 
 Route::get('/events/create', [EventController::class, 'create']);
 
+Route::post('/events', [EventController::class, 'store']);
+
+
 Route::get('/contatos', function () {
     return view('contact');
 });
