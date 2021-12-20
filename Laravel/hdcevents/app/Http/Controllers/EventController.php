89d4117f96906973062612655f028d62d16a4b06
Vítor Alexandre <devper.vnk@gmail.com>
2021-12-20 18:44:03 -0300

    .

--- Laravel/hdcevents/app/Http/Controllers/EventController.php
+++ Laravel/hdcevents/app/Http/Controllers/EventController.php
@@ -41,6 +41,11 @@ class EventController extends Controller{
     }
 
 
+    public function show($id){
+        $event = Event::findOrFail($id);
+
+        return view('events.show' , ['event' => $event]);
+    }
 
 
 
---
+++ Laravel/hdcevents/resources/views/events/show.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.main')
+@section('title', "$event->title | HDC Events")
+@section('content')
+
+<div class="col-md-10 offset-md-1">
+    <div class="row">
+        <div id="image-container" class="col-md-6">
+            <img src="/img/events/{{$event->image}}" class="img-fluid" alt="{{$event->title}}">
+        </div>
+        <div id="info-container" class="col-md-6">
+            <h1>{{$event->title}}</h1>
+            <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{$event->city}}</p>
+            <p class="events-participants"><ion-icon name="people-outline"></ion-icon> X Partipantes</p>
+            <p class="event-owner"><ion-icon name="star-outline"></ion-icon> Dono do Evento</p>
+            <a href="#" class="btn btn-primary" id="event-submit"> Confirmar Presença</a>
+        </div>
+        <div class="col-md-12" id="descripton-container">
+            <h4>Sobre o evento:</h4>
+            <p class="event-description">{{$event->description}}</p>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
+
+
+@endsection
